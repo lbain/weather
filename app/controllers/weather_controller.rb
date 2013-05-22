c6fe023bde5c2c58003959b20a46d2950255abb4
Lucy Bain <lucybain@gmail.com>
2013-05-22 12:03:19 +1000

    very simple working version of the app - no frills

---
+++ app/controllers/weather_controller.rb
@@ -0,0 +1,45 @@
+class WeatherController < ApplicationController
+
+  def zipcode_inquiry
+  end
+
+  def show_weather
+    # check for form param
+    unless params[:zipcode_form] && params[:zipcode_form][:zipcode]
+      flash[:error] = "No zipcode given"
+      redirect_to zipcode_inquiry_path
+      return
+    end
+    zipcode = params[:zipcode_form][:zipcode]
+    # check for correct format of the zipcode
+    unless !!(zipcode =~ /\d{5}/)
+      flash[:error] = "Invalid zip code format."
+      redirect_to zipcode_inquiry_path
+      return
+    end
+
+    # everything was correct with the zipcode formatting
+    wunderground = Wunderground.new("ed044d75b91fb500")
+    wunderground.throws_exceptions = true
+    begin
+      #try to get the conditions for the given zipcode
+      conditions = wunderground.conditions_for(zipcode)
+    rescue Exception => e
+      debugger
+      if !!(e.message =~ /querynotfound/)
+        # the user entered an invalid zipcode
+        flash[:error] = "Zipcode not found."
+      else
+        # something went wrong with the call (most likely a time out) - give generic error message
+        flash[:error] = "Error getting weather."
+      end
+      redirect_to zipcode_inquiry_path
+      return
+    end
+
+    # everything was correct about the zipcode info, we now have the data back from the api
+    @city = conditions["current_observation"]["display_location"]["city"]
+    @state = conditions["current_observation"]["display_location"]["state_name"]
+    @temp_f = conditions["current_observation"]["temp_f"]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Weather::Application.routes.draw do
 
-  match '/weather' => 'weather#zipcode_inquiry', via: [:get], :as => :zipcode_inquiry
-  match '/weather' => 'weather#show_weather', via: [:post], :as => :new_zipcode
+  match '/' => 'weather#zipcode_inquiry', via: [:get], :as => :zipcode_inquiry
+  match '/' => 'weather#show_weather', via: [:post], :as => :new_zipcode
 
 end

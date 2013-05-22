b99819f4b978094041f3d53a9d3958f27e6159ae
Lucy Bain <lucybain@gmail.com>
2013-05-22 11:54:15 +1000

    basic setup for weather app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weather::Application.config.secret_token = 'bd7feaca172fa0618b6d09b742d547878de06d27bbfaf886417e13c51e839bec49213c54e1f6885a2f0630c8e6760fe3c6b3e337008d3c5d3f489c464fa8a20b'

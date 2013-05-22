67aed5be90ac297785e5fb42bcfea099a5430b09
Lucy Bain <lucybain@gmail.com>
2013-05-22 15:23:26 +1000

    allow any path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Weather::Application.routes.draw do
 
-  match '/' => 'weather#zipcode_inquiry', via: [:get], :as => :zipcode_inquiry
+  match '(*all)' => 'weather#zipcode_inquiry', via: [:get], :as => :zipcode_inquiry
   match '/' => 'weather#show_weather', via: [:post], :as => :new_zipcode
 
 end
